8a49c45830ee4a75aacb8c5f6dca866bb9a76e7e
coddeys <coddeys@gmail.com>
2018-05-29 07:04:01 +0200

    Nix-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -488,6 +488,10 @@ scroll-preserve-screen-position 1)
   (global-set-key (kbd "C-'") 'ace-window)
   (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l)))
 
+;; nix-mode
+(use-package nix-mode
+  :ensure t)
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; dired                                                                  ;;
@@ -507,7 +511,7 @@ scroll-preserve-screen-position 1)
     ("~/todo/day.org" "~/todo/2018.org" "~/projects/gs/jira/gs1081.org" "~/projects/gs/jira/gs-1012.org")))
  '(package-selected-packages
    (quote
-    (indium indium-scratch stylus-mode org-pomodoro web-beautify octave-mode diff-hl rainbow-mode rainbow-delimiters restclient alchemist-mode alchemist monokai-theme emacsql-psql elm-mode zeal-at-point helm-dash org-jira jira-markup-mode slack hindent scss-mode sass-mode markdown-mode markdown git-gutter anzu flycheck-elm docker dockerfile-mode railscasts-reloaded-theme railscasts-theme anti-zenburn-theme nodejs-repl moz haskell-mode slim-mode zenburn-theme yaml-mode w3m use-package smartparens ruby-refactor ruby-block rubocop robe restclient-test projectile-rails multiple-cursors multi-term markdown-preview-mode magit log4j-mode json-mode js2-mode ivy-hydra highlight-indentation helm-projectile helm-ag google-translate flycheck fill-column-indicator discover counsel company-web ace-window)))
+    (nix-mode indium indium-scratch stylus-mode org-pomodoro web-beautify octave-mode diff-hl rainbow-mode rainbow-delimiters restclient alchemist-mode alchemist monokai-theme emacsql-psql elm-mode zeal-at-point helm-dash org-jira jira-markup-mode slack hindent scss-mode sass-mode markdown-mode markdown git-gutter anzu flycheck-elm docker dockerfile-mode railscasts-reloaded-theme railscasts-theme anti-zenburn-theme nodejs-repl moz haskell-mode slim-mode zenburn-theme yaml-mode w3m use-package smartparens ruby-refactor ruby-block rubocop robe restclient-test projectile-rails multiple-cursors multi-term markdown-preview-mode magit log4j-mode json-mode js2-mode ivy-hydra highlight-indentation helm-projectile helm-ag google-translate flycheck fill-column-indicator discover counsel company-web ace-window)))
  '(sp-ignore-modes-list (quote (minibuffer-inactive-mode shell-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- .zshrc
+++ .zshrc
@@ -7,3 +7,7 @@ DISABLE_AUTO_UPDATE="true"
 plugins=(git archlinux tmux)
 
 source $ZSH/oh-my-zsh.sh
+
+export PATH=~/.npm-global/bin:$PATH
+
+if [ -e /home/coddeys/.nix-profile/etc/profile.d/nix.sh ]; then . /home/coddeys/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer

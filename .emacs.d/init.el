68e1cefc3f2259200c1d8f5aefd4de7350487b76
coddeys <coddeys@gmail.com>
2018-07-09 20:06:55 +0200

    use local eslint from node_modules before global

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -214,6 +214,19 @@ scroll-preserve-screen-position 1)
   :modes (enh-ruby-mode ruby-mode)
   )
 
+;; use local eslint from node_modules before global
+;; http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
 (use-package company
   :ensure t
   :config
